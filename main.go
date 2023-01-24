da8dbae59fb4e279eb99d1226b487c3b8c4feb5b
oscarneiva <oscarneivaeneto@gmail.com>
2023-01-24 18:30:49 -0300

    Final version 1.0. Program reads all csv files in data folder and generate the recuperacao letters.

--- main.go
+++ main.go
@@ -12,31 +12,41 @@ func main() {
 }
 
 func readFile() {
-	file, err := os.Open("./data/Recuperacao.csv")
+	dirReader, err := os.Open("./data/")
 	if err != nil {
 		fmt.Println("Error ", err)
 	}
-	reader := csv.NewReader(file)
-	data, err := reader.ReadAll()
-	if err == nil {
+	files, err := dirReader.ReadDir(0)
+	if err != nil {
 		fmt.Println("Error ", err)
 	}
-	for _, line := range data[1:] {
-		if line != nil {
-			letter := Letter{
-				Subject: line[0],
-				Date:    time.Now(),
-				Period:  line[1],
-				Student: line[2],
-				Class:   line[3],
-				Pass:    checkPass(line[4]),
-			}
-			LetterGen(letter)
-		} else {
-			break
+	for i := range files {
+		file, err := os.Open("./data/" + files[i].Name())
+		if err != nil {
+			fmt.Println("Error ", err)
 		}
+		reader := csv.NewReader(file)
+		data, err := reader.ReadAll()
+		if err == nil {
+			fmt.Println("Error ", err)
+		}
+		for _, line := range data[1:] {
+			if line != nil {
+				letter := Letter{
+					Subject: line[0],
+					Date:    time.Now(),
+					Period:  line[1],
+					Student: line[2],
+					Class:   line[3],
+					Pass:    checkPass(line[4]),
+				}
+				LetterGen(letter)
+			} else {
+				break
+			}
+		}
+		file.Close()
 	}
-	file.Close()
 }
 
 func checkPass(letter string) bool {
